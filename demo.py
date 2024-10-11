a50e7074234e026bd3cf44743846ae50967cc9eb
James Edmondson <jedmondson@gmail.com>
2024-10-11 12:59:13 -0700

    * Updated demo.py to only require a model file (the weights file) and build cfg and names from that * Updated requirements to require gpu onnxruntime. We're not really trying to support cpu

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 '''
-@Time          : 20/04/25 15:49
-@Author        : huguanghao
-@File          : demo.py
-@Noice         :
+@Time      : 20/04/25 15:49
+@Author    : huguanghao
+@File      : demo.py
+@Noice     :
 @Modificattion :
-    @Author    :
-    @Time      :
-    @Detail    :
+  @Author  :
+  @Time    :
+  @Detail  :
 '''
 
 # import sys
@@ -19,148 +19,150 @@ from tool.torch_utils import *
 from tool.darknet2pytorch import Darknet
 import torch
 import argparse
+import os
+import cv2
 
-"""hyper parameters"""
-use_cuda = True
+def detect_cv2(options):
+  m = Darknet(options.config_file)
 
-def detect_cv2(cfgfile, weightfile, imgfile):
-    import cv2
-    m = Darknet(cfgfile)
+  m.print_network()
+  m.load_weights(options.model)
 
-    m.print_network()
-    m.load_weights(weightfile)
-    print('Loading weights from %s... Done!' % (weightfile))
+  print(f'Loading weights from {options.model}... Done!')
 
-    if use_cuda:
-        m.cuda()
+  if options.use_cuda:
+    m.cuda()
 
-    num_classes = m.num_classes
-    if num_classes == 20:
-        namesfile = 'data/voc.names'
-    elif num_classes == 80:
-        namesfile = 'data/coco.names'
-    else:
-        namesfile = 'data/x.names'
-    class_names = load_class_names(namesfile)
+  class_names = load_class_names(options.names_file)
 
-    img = cv2.imread(imgfile)
+  img = cv2.imread(options.image)
+  sized = cv2.resize(img, (m.width, m.height))
+  sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+  for i in range(2):
+    start = time.time()
+    boxes = do_detect(m, sized, 0.4, 0.6, options.use_cuda)
+    finish = time.time()
+    if i == 1:
+      print('%s: Predicted in %f seconds.' % (options.image, (finish - start)))
+
+  plot_boxes_cv2(img, boxes[0], savename='predictions.jpg', class_names=class_names)
+
+
+def detect_cv2_camera(options):
+  import cv2
+  m = Darknet(options.config_file)
+
+  m.print_network()
+  if options.torch:
+    m.load_state_dict(torch.load(options.model))
+  else:
+    m.load_weights(options.model)
+  print('Loading weights from %s... Done!' % (options.model))
+
+  if options.use_cuda:
+    m.cuda()
+
+  cap = cv2.VideoCapture(0)
+
+  cap.set(3, 1280)
+  cap.set(4, 720)
+
+  print("Starting the YOLO loop...")
+
+  options.names_file = os.path.splitext(options.model)[0] + ".names"
+
+  class_names = load_class_names(options.names_file)
+
+  while True:
+    ret, img = cap.read()
     sized = cv2.resize(img, (m.width, m.height))
     sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
 
-    for i in range(2):
-        start = time.time()
-        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
-        finish = time.time()
-        if i == 1:
-            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
+    start = time.time()
+    boxes = do_detect(m, sized, 0.4, 0.6, options.use_cuda)
+    finish = time.time()
+    print('Predicted in %f seconds.' % (finish - start))
 
-    plot_boxes_cv2(img, boxes[0], savename='predictions.jpg', class_names=class_names)
+    result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=class_names)
 
+    cv2.imshow('Yolo demo', result_img)
+    cv2.waitKey(1)
 
-def detect_cv2_camera(cfgfile, weightfile):
-    import cv2
-    m = Darknet(cfgfile)
-
-    m.print_network()
-    if args.torch:
-        m.load_state_dict(torch.load(weightfile))
-    else:
-        m.load_weights(weightfile)
-    print('Loading weights from %s... Done!' % (weightfile))
-
-    if use_cuda:
-        m.cuda()
-
-    cap = cv2.VideoCapture(0)
-    # cap = cv2.VideoCapture("./test.mp4")
-    cap.set(3, 1280)
-    cap.set(4, 720)
-    print("Starting the YOLO loop...")
-
-    num_classes = m.num_classes
-    if num_classes == 20:
-        namesfile = 'data/voc.names'
-    elif num_classes == 80:
-        namesfile = 'data/coco.names'
-    else:
-        namesfile = 'data/x.names'
-    class_names = load_class_names(namesfile)
-
-    while True:
-        ret, img = cap.read()
-        sized = cv2.resize(img, (m.width, m.height))
-        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-
-        start = time.time()
-        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
-        finish = time.time()
-        print('Predicted in %f seconds.' % (finish - start))
-
-        result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=class_names)
-
-        cv2.imshow('Yolo demo', result_img)
-        cv2.waitKey(1)
-
-    cap.release()
+  cap.release()
 
 
-def detect_skimage(cfgfile, weightfile, imgfile):
-    from skimage import io
-    from skimage.transform import resize
-    m = Darknet(cfgfile)
+def detect_skimage(options):
+  from skimage import io
+  from skimage.transform import resize
+  m = Darknet(options.config_file)
 
-    m.print_network()
-    m.load_weights(weightfile)
-    print('Loading weights from %s... Done!' % (weightfile))
+  m.print_network()
+  m.load_weights(options.model)
+  print('Loading weights from %s... Done!' % (options.model))
 
-    if use_cuda:
-        m.cuda()
+  if options.use_cuda:
+    m.cuda()
 
-    num_classes = m.num_classes
-    if num_classes == 20:
-        namesfile = 'data/voc.names'
-    elif num_classes == 80:
-        namesfile = 'data/coco.names'
-    else:
-        namesfile = 'data/x.names'
-    class_names = load_class_names(namesfile)
+  num_classes = m.num_classes
+  if num_classes == 20:
+    options.names_file = 'data/voc.names'
+  elif num_classes == 80:
+    options.names_file = 'data/coco.names'
+  else:
+    options.names_file = 'data/x.names'
+  class_names = load_class_names(options.names_file)
 
-    img = io.imread(imgfile)
-    sized = resize(img, (m.width, m.height)) * 255
+  img = io.imread(options.image)
+  sized = resize(img, (m.width, m.height)) * 255
 
-    for i in range(2):
-        start = time.time()
-        boxes = do_detect(m, sized, 0.4, 0.4, use_cuda)
-        finish = time.time()
-        if i == 1:
-            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
+  for i in range(2):
+    start = time.time()
+    boxes = do_detect(m, sized, 0.4, 0.4, options.use_cuda)
+    finish = time.time()
+    if i == 1:
+      print('%s: Predicted in %f seconds.' % (options.image, (finish - start)))
 
-    plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
+  plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
 
 
-def get_args():
-    parser = argparse.ArgumentParser('Test your image or video by trained model.')
-    parser.add_argument('-cfgfile', type=str, default='./cfg/yolov4.cfg',
-                        help='path of cfg file', dest='cfgfile')
-    parser.add_argument('-weightfile', type=str,
-                        default='./checkpoints/Yolov4_epoch1.pth',
-                        help='path of trained model.', dest='weightfile')
-    parser.add_argument('-imgfile', type=str,
-                        default='./data/mscoco2017/train2017/190109_180343_00154162.jpg',
-                        help='path of your image file.', dest='imgfile')
-    parser.add_argument('-torch', type=bool, default=false,
-                        help='use torch weights')
-    args = parser.parse_args()
-
-    return args
+def parse_args(args):
+  parser = argparse.ArgumentParser(
+    description="Runs inference with pytorch rather than yolov4 weights",
+    add_help=True
+  )
+  parser.add_argument('--cf', '--config-file', action='store',
+            help='path of cfg file', dest='config_file')
+  parser.add_argument('--nf', '--names-file', action='store',
+            help='path of cfg file', dest='names_file')
+  parser.add_argument('-m', '--model', action='store',
+            help='path of trained model.', dest='model')
+  parser.add_argument('-i', '--image', action='store',
+            default='./data/mscoco2017/train2017/190109_180343_00154162.jpg',
+            help='path of your image file.', dest='image')
+  parser.add_argument('--uc', '--use-cuda',
+            action='store_true',
+            help='if true, use CUDA. Otherwise, CPU.', dest='use_cuda')
+  
+  return parser.parse_args(args)
 
 
 if __name__ == '__main__':
-    args = get_args()
-    if args.imgfile:
-        detect_cv2(args.cfgfile, args.weightfile, args.imgfile)
-        # detect_imges(args.cfgfile, args.weightfile)
-        # detect_cv2(args.cfgfile, args.weightfile, args.imgfile)
-        # detect_skimage(args.cfgfile, args.weightfile, args.imgfile)
-    else:
-        detect_cv2_camera(args.cfgfile, args.weightfile)
+  options = parse_args(sys.argv[1:])
+
+  if options.model:
+    
+    basename = os.path.splitext(options.model)[0]
+
+    if not options.names_file:
+      options.names_file = f"{basename}.names"
+    
+    if not options.config_file:
+      options.config_file = f"{basename}.cfg"
+
+  if options.image:
+    detect_cv2(options)
+  else:
+    detect_cv2_camera(options)
+
+
