a806807531e076736ee5ee20cafaed1f98d891da
James Edmondson <james@koshee.ai>
2025-08-15 17:04:41 -0700

    * Added support for OpenVino for Intel Arc GPUs * Updated to version 0.6.1

---
+++ scripts/install_vino.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${SCRIPTS_DIR}")" && pwd)"
+SELF="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+
+sudo apt install python3 python3-pip python3-venv
+
+cd $PROJECT_DIR
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -e ".[vino]"
+
